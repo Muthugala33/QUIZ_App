8b45824ba29b87c79e79a2224f0b968921f495a2
Pasindu keshan <pasikeshan21.com>
2025-03-15 12:22:37 +0530

    implement02

--- src/main/java/edu/icet/controller/ExamController.java
+++ src/main/java/edu/icet/controller/ExamController.java
@@ -2,6 +2,9 @@ package edu.icet.controller;
 
 
 import edu.icet.dto.Exam;
+import edu.icet.service.ExamService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/exam")
+@RequiredArgsConstructor
+
 public class ExamController {
+
+   final ExamService service;
 
     @PostMapping("/create-exam")
     public void createExam(@RequestBody Exam exam){
 
+        service.createExam(exam);
 
     }
 }
